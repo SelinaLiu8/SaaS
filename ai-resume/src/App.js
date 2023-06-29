b5cf985f82b70273c6724308b74094704bb2d450
aavin@umich.edu <aavin@umich.edu>
2023-06-28 22:21:58 -0400

    sign in working and SDK in the right place

--- ai-resume/src/App.js
+++ ai-resume/src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import { getAuth, GoogleAuthProvider, onAuthStateChanged, signInWithPopup, signOut as signOutOfFirebase } from 'firebase/auth';
+import SignInButton from './SignInButton';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const auth = getAuth();
+const provider = new GoogleAuthProvider();
 
-export default App;
+const MyComponent = () => {
+    const [user, setUser] = useState(null);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, user => {
+            if (user) {
+                setUser(user);
+            } else {
+                setUser(null);
+            }
+        });
+
+        // Cleanup subscription on unmount
+        return () => unsubscribe();
+    }, []);
+
+    const signIn = async () => {
+        try {
+            await signInWithPopup(auth, provider);
+        } catch (error) {
+            setError(error.message);
+        }
+    };
+
+    const signOut = async () => {
+        try {
+            await signOutOfFirebase(auth);
+        } catch (error) {
+            setError(error.message);
+        }
+    };
+
+    if (error) {
+        return (
+            <div>
+                <p>Error: {error}</p>
+                <button onClick={() => setError(null)}>Clear Error</button>
+            </div>
+        );
+    }
+
+    if (user) {
+        return (
+            <div>
+                <button onClick={signOut}>Sign Out</button>
+                <h3>Hello {user.displayName}!</h3>
+                <p>User ID: {user.uid}</p>
+            </div>
+        );
+    } else {
+        return (
+            <div>
+                <SignInButton signIn={signIn} /> {/* Pass the signIn function as a prop to the SignInButton */}
+            </div>
+        );
+    }
+};
+
+export default MyComponent;

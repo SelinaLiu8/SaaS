79733f15dd25f26aa8693b9a244b7b95b5de02d7
aavin@umich.edu <aavin@umich.edu>
2023-07-06 18:48:28 -0400

    init navbar

--- ai-resume/client/src/pages/_app.tsx
+++ ai-resume/client/src/pages/_app.tsx
@@ -1,6 +1,10 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import Navbar from '../components/NavBar';
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      <Navbar />
+      <Component {...pageProps} />
+    </>
+  );
 }

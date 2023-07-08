44964c9f6dcd9158f7b10e93fa86a56469bfc76f
aavin@umich.edu <aavin@umich.edu>
2023-07-07 21:15:54 -0400

    user icon working

--- ai-resume/client/components/navbar/NavBar.tsx
+++ ai-resume/client/components/navbar/NavBar.tsx
@@ -2,11 +2,11 @@ import Link from 'next/link';
 import Image from 'next/image';
 import { useContext } from 'react';
 import { UserContext } from '../../lib/context';
+import { useUserData } from '../../lib/hooks'; // Assuming the hook is defined here
 
 // Top navbar
 export default function Navbar({ children }) {
-  const user = useContext(UserContext);
-  console.log(user);
+  const { user, username } = useUserData();
 
   return (
     <nav className="navbar">
@@ -18,7 +18,7 @@ export default function Navbar({ children }) {
         </li>
 
         {/* user is signed-in and has username */}
-        {user.user && (
+        {user && (
           <>
             <li className="push-left">
               <Link href="/admin">
@@ -26,20 +26,17 @@ export default function Navbar({ children }) {
               </Link>
             </li>
             <li>
-              <Link href={`/${user}`}>
-                {user && user.photoURL && <Image src={user.photoURL} alt={user} width={500} height={500}/>}
+              <Link href={`/${username}`}>
+                {user?.photoURL && (
+                  <Image src={user.photoURL} alt={`${username}'s profile picture`} width={500} height={500} />
+                )}
               </Link>
             </li>
-            <li>
-            <Link href="/enter">
-              <button className="btn-blue">Log out</button>
-            </Link>
-          </li>
           </>
         )}
 
         {/* user is not signed OR has not created username */}
-        {!user.user && (
+        {!user && (
           <li>
             <Link href="/enter">
               <button className="btn-blue">Log in</button>

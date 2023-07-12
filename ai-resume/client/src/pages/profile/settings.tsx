fff9596daaf4317f208319b57128d875eaf8fc9e
aavin@umich.edu <aavin@umich.edu>
2023-07-12 18:02:28 -0400

    admin shit

--- ai-resume/client/src/pages/profile/settings.tsx
+++ ai-resume/client/src/pages/profile/settings.tsx
@@ -1,23 +1,13 @@
 import { useContext } from 'react';
+import Link from 'next/link';
 import AuthCheck from '../../../components/AuthCheck';
 import { useUserData } from '../../../lib/hooks';
 import UserProfileImage from '../../../components/ProfileImg';
 import ResetPasswordButton from '../../../components/ResetPassword';
 import ResumeUploader from '../../../components/ResumeUploader';
-import { useDocumentData } from 'react-firebase-hooks/firestore';
-import { getFirestore, doc } from 'firebase/firestore';
-
-// Initialize Firestore
-const db = getFirestore();
 
 export default function AdminPage(props) {
     const { user, username } = useUserData();
-
-    // Firestore document to fetch
-    const userDoc = user && doc(db, "users", user.uid);
-  
-    // Fetch user data from Firestore
-    const [userData] = useDocumentData(userDoc, {idField: 'id'});
 
     return (
         <main>
@@ -25,12 +15,14 @@ export default function AdminPage(props) {
                 <p>Welcome to the Admin!</p>
                 <UserProfileImage user={user} />
                 <p>Email: {username || user?.email}</p>
-                {userData && <p>Name: {userData?.firstName} {userData?.lastName}</p>}
+                {
+                    user?.providerData[0]?.providerId === 'password' && <ResetPasswordButton user={user} />
+                }
                 {
                   user && <ResumeUploader user={user} />
                 }
-                <button>
-                    <a href="/profile/settings">Settings</a>
+                <button> 
+                    <a href="/profile/" className="button-style">Back</a>
                 </button>
             </AuthCheck>
         </main>

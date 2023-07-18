7c23fc8a464115b466fb8cb1934555f624471b1f
selinal <selnal@umich.edu>
2023-07-18 00:00:00 -0400

    login nav fixed

--- ai-resume/client/src/pages/_app.tsx
+++ ai-resume/client/src/pages/_app.tsx
@@ -4,12 +4,16 @@ import NavBar from '../../components/NavBar'
 import { UserContext } from '../../lib/context';
 import { useUserData } from '../../lib/hooks';
 import { Toaster } from 'react-hot-toast';
+import { useRouter } from 'next/router';
 
 export default function App({ Component, pageProps }: AppProps) {
   const userData = useUserData();
+  const isLoginPage = useRouter().pathname === '/enter';
+  const shouldShowHeader = !isLoginPage;
+
   return (
     <UserContext.Provider value={userData}>
-      <NavBar/>
+      {shouldShowHeader && <NavBar />}
         <Component {...pageProps} />
       <Toaster />
     </UserContext.Provider>

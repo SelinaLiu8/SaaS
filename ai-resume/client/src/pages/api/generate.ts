e9d634751bef16b84f285e32a7b54970d290431c
aavin@umich.edu <aavin@umich.edu>
2023-08-10 12:42:59 -0400

    removed log

--- ai-resume/client/src/pages/api/generate.ts
+++ ai-resume/client/src/pages/api/generate.ts
@@ -54,7 +54,6 @@ export default async function handler(req, res) {
       }],
     });
     res.status(200).json({ message: chatCompletion.data.choices[0].message });
-    console.log(chatCompletion.data.choices[0].message);
   } catch (error) {
     res.status(500).json({ error: error.toString(), stack: error.stack });
   }

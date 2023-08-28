e91cb589841be691b51d53046960efc8a1314a18
selinal <selnal@umich.edu>
2023-08-28 18:13:26 -0400

    css

--- ai-resume/client/src/pages/profile/index.tsx
+++ ai-resume/client/src/pages/profile/index.tsx
@@ -34,14 +34,20 @@ export default function AdminPage(props) {
                         <p>credit</p>
                     </div>
                 </div>
-                <p>Welcome to the Admin!</p>
                 {userData && <p>Name: {userData?.firstName} {userData?.lastName}</p>}
-                {
-                  user && <ResumeUploader user={user} />
-                }
-                <button>
-                    <Link href="/profile/settings">Settings</Link>
-                </button>
+                <div className="custom-shape-divider-top-1689553257">
+                    <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
+                        <path d="M985.66,92.83C906.67,72,823.78,31,743.84,14.19c-82.26-17.34-168.06-16.33-250.45.39-57.84,11.73-114,31.07-172,41.86A600.21,600.21,0,0,1,0,27.35V120H1200V95.8C1132.19,118.92,1055.71,111.31,985.66,92.83Z" class="shape-fill"></path>
+                    </svg>
+                </div>
+                <div className='profile-bottom'>
+                    {
+                    user && <ResumeUploader user={user} />
+                    }
+                    <button>
+                        <Link href="/profile/settings">Settings</Link>
+                    </button>
+                </div>
             </AuthCheck>
         </main>
     );
